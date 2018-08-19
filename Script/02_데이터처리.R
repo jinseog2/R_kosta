46052be4dce272075c5dc25d41a379bedbd19e92
taecjee <taecjee@naver.com>
2018-08-19 10:44:05 +0900

    Update 02_데이터처리.R

--- Script/02_데이터처리.R
+++ Script/02_데이터처리.R
@@ -1,4 +1,4 @@
-## 69
+## View
 
 dim(iris)
 names(iris)
@@ -6,7 +6,7 @@ str(iris)
 
 attributes(iris)
 
-## 70
+## Explore
 
 summary(iris)
 
@@ -17,7 +17,7 @@ quantile(iris$Sepal.Length, c(0.1, 0.3, 0.65))
 var(iris$Sepal.Length)
 hist(iris$Sepal.Length)
 
-## 71
+## sorting
 
 vec1 <- c(3, 2, 5, 1, 4)
 sort(vec1)
@@ -37,72 +37,7 @@ data('iris')
 names(iris)
 iris.ordered <- iris[order(iris$Sepal.Length, iris$Sepal.Width), ]
 
-## 72
-
-sample <- data.frame(var1 = rep(c('Male', 'Female'), 10), var2 = rep(c('A', 'B', 'C', 'D')))
-example.table <- table(sample$var1, sample$var2)
-example.table
-
-data(iris)
-aggregate(iris$Sepal.Length, by = list(iris$Species), FUN = 'mean')
-
-aggregate(iris$Sepal.Length ~ iris$Species, FUN = 'mean')
-aggregate(Sepal.Length ~ Species, data = iris, FUN = 'mean')
-iris$letter <- letters[1:2]
-aggregate(Sepal.Length ~ Species + letter, data = iris, FUN = 'mean')
-aggregate(cbind(Sepal.Length, Sepal.Width) ~ Species, data = iris, FUN = 'mean')
-data(iris)
-aggregate(. ~ Species, data = iris, FUN = 'mean')
-
-## 73
-
-table(iris$Species)
-pie(table(iris$Species))
-barplot(table(iris$Species))
-
-## 75
-
-gregexpr('[a-z]', 'string 01 A')
-gregexpr('[a-z0]', 'string 01 A')
-gregexpr('[[:alnum:]]', 'string 01 A')
-gregexpr('.', 'string 01 A')
-
-## 76
-
-gregexpr('\n', 'string 01 
-         A')
-gregexpr('[^a-z]', 'string 01 A')
-
-gregexpr('r|n', 'string 01 A')
-
-gregexpr('[a-z]{2,4}', 'string 01 A')
-
-## 77
-
-grep('tri', c('string', 'triangle'), value = T)
-grep('^tri', c('string', 'triangle'), value = T)
-grep('^triangle', c('string', 'triangle', 'triangles'), value = T)
-grep('^triangle$', c('string', 'triangle', 'triangles'), value = T)
-
-grep('^pri|tri', c('triangle', 'triangles', 'price', 'priority', 'string'), value = T)
-grep('^(pri|tri)', c('triangle', 'triangles', 'price', 'priority', 'string'), value = T)
-
-gregexpr('\\?', 'what is this?')
-
-## 78
-
-numbers <- c('+1 456-2341', '5342673', '55578274982', '74683029873', '25', '+442 5421611')
-grep('^(\\+[0-9]+\\s)?[0-9]{3}\\-?[0-9]{4}$', numbers, value = T)
-grep('^(\\+[0-9]+\\s)?[0-9]{3}\\-?[0-9]{4}', numbers, value = T)
-
-example <- 'This is a text. What is this exactly? A text. Are you sure?'
-greps <- gregexpr('\\s[A-Z]{1}[^\\?\\.]*\\?', example)
-regmatches(example, greps)
-
-greps <- gregexpr('\\s[A-Z]{1}.*\\?', example)
-regmatches(example, greps)
-
-## 79
+## apply function
 
 sample.list <- list(a = runif(100, 0, 1), b = runif(500, 0, 100), c = runif(35, 0, 200))
 sapply(sample.list, quantile, probs = 0.75)
@@ -115,17 +50,85 @@ x <- as.numeric(c(1:2000000))
 ptm <- proc.time()
 for (i in x) x[i] <- x[i] * x[i]
 proc.time() - ptm
+       
 ptm <- proc.time()
 x <- sapply(x, function(x) x * x)
 proc.time() - ptm
+            
+## rbind
 
-## 80
+rbind(c(1, 2, 3), c(4, 5, 6))
+rbind(c(1, 2, 3), c(4, 5))    # 오류
+rbind(c(1, 2, 3), c(4, 5, ))  # 오류
+rbind(c(1, 2, 3), c(4, 5, NA))
+
+x <- data.frame(id = c(1, 2), name = c('a', 'b'))
+x
+y <- rbind(x, c(3, 'c'))  ## 오류 요인 수준 다름
+y <- rbind(x, c(3, 'b'))
+y
+
+x <- data.frame(id = c(1, 2), name = c('a', 'b'), stringsAsFactors = F)
+y <- rbind(x, c(3, 'c'))
+y
+
+## cbind
+            
+cbind(c(1,2,3), c(4,5,6))
+
+x <- data.frame(id = c(1, 2), name = c('a', 'b'), stringsAsFactors = F)
+y <- cbind(x, greek = c('alpha', 'beta'))
+y
+summary(y)
+y <- cbind(x, greek = c('alpha', 'beta'), stringsAsFactors=F)
+y
+summary(y)
+
+y$korean <- c('ㄱ', 'ㄴ')
+y
+summary(y)
+            
+## merge
+            
+x <- data.frame(names=c('a','b','c'), math=c(40,50,70))
+y <- data.frame(names=c('a','b','c'), english=c(60,40,80))
+merge(x, y)
+cbind(x, y)
+
+y <- data.frame(names=c('a','b','c', 'd'), english=c(60,40,80,90))
+merge(x, y)
+merge(x, y, all=T)
+            
+## split, subset
+            
+idx <- sample(2, nrow(iris), replace = T, prob = c(0.3, 0.7))
+idx
+split(iris[idx==1,], iris[idx==1,]$Species)
+
+subset(iris, Species == 'setosa')
+subset(iris, Species == 'setosa' & Sepal.Length > 5.1)
+subset(iris, select = c(Sepal.Length, Species), Species == 'setosa' & Sepal.Length > 5.1)
+subset(iris, select = -c(Sepal.Length, Species), Species == 'setosa' & Sepal.Length > 5.1)
+         
+## which
+            
+iris$Sepal.Length > 6.0
+which(iris$Sepal.Length > 6.0)
+iris[which(iris$Sepal.Length > 6.0),]
+
+which.min(iris$Sepal.Length)
+iris[which.min(iris$Sepal.Length),]
+
+which.max(iris$Sepal.Length)
+iris[which.max(iris$Sepal.Length),]            
+            
+## plyr
 
 library(plyr)
 ddply(iris, .(Species), summarize, indicator1 = quantile(Sepal.Length, 0.75), indicator2 = sum(Sepal.Width)/sum(Petal.Length))
 dlply(iris, .(Species), summarize, indicator1 = quantile(Sepal.Length, 0.75), indicator2 = sum(Sepal.Width)/sum(Petal.Length))
 
-## 81
+## reshape2
 
 library(reshape2)
 data(iris)
@@ -138,8 +141,30 @@ dcast(m.iris, variable ~ Species, fun.aggregate = function(x) sum(x + 2))
 
 class(acast(m.iris, variable ~ Species, fun.aggregate = sum))
 class(dcast(m.iris, variable ~ Species, fun.aggregate = sum))
+      
+## aggregate
+      
+aggregate(Sepal.Width ~ Species, iris, mean)
+testDF <- data.frame(v1 = c(1,3,5,7,8,3,5,NA,4,5,7,9),
+                     v2 = c(11,33,55,77,88,33,55,NA,44,55,77,99) )
+by1 <- c("red", "blue", 1, 2, NA, "big", 1, 2, "red", 1, NA, 12)
+by2 <- c("wet", "dry", 99, 95, NA, "damp", 95, 99, "red", 99, NA, NA)
+aggregate(x = testDF, by = list(by1, by2), FUN = "mean")      
+      
+## table
+      
+table(by1, by2)
+iris.Species.sample <- sample(iris$Species)
+table(iris$Species, iris.Species.sample)
+      
+## summary chart
+      
+table(iris$Species)
+pie(table(iris$Species))
+      
+barplot(table(iris$Species))      
 
-## 82
+## save to files
 
 pdf('myPlot.pdf')
 x <- 1:50
