c6caadc677828eb57b0296bd1bc550f329095365
taecjee <taecjee@naver.com>
2018-08-19 12:43:10 +0900

    Update 05_군집화.R

--- Script/05_군집화.R
+++ Script/05_군집화.R
@@ -1,18 +1,17 @@
-## 11
+## K-means clustering
 
 set.seed(8953)
 iris2 <- iris
 iris2$Species <- NULL
 (kmeans.result <- kmeans(iris2, 3))
 
-## 12
-
+# Result
 table(iris$Species, kmeans.result$cluster)
 
 plot(iris2[c("Sepal.Length", "Sepal.Width")], col=kmeans.result$cluster)
 points(kmeans.result$centers[,c("Sepal.Length", "Sepal.Width")], col = 1:3, pch = 8, cex = 2)
 
-## 15
+## Clustering with pamk
 
 library(fpc)
 pamk.result <- pamk(iris2)
@@ -21,26 +20,24 @@ pamk.result$nc
 
 table(pamk.result$pamobject$clustering, iris$Species)
 
-## 16
-
+## Result
 layout(matrix(c(1,2), 1, 2))
 plot(pamk.result$pamobject)
 layout(matrix(1))
 
-## 17
+## Clustering with pam
 
 library(cluster)
 
 pam.result <- pam(iris2, 3)
 table(pam.result$clustering, iris$Species)
 
-## 18
-
+## Result
 layout(matrix(c(1,2), 1, 2))
 plot(pam.result)
 layout(matrix(1))
 
-## 21
+## Hierarchical Clustering
 
 set.seed(2835)
 
@@ -49,12 +46,9 @@ irisSample <- iris[idx,]
 
 irisSample$Species <- NULL
 
-hc <- hclust(dist(irisSample), method = 'ave')
+(hc <- hclust(dist(irisSample), method = 'ave'))
 
-hc
-
-## 22
-
+## Result
 plot(hc, hang = -1, labels = iris$Species[idx])
 
 rect.hclust(hc, k = 3)
