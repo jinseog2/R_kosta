11d4f7934147e001db149f6febb1b29ee1b88fdf
taecjee <taecjee@naver.com>
2018-08-19 11:33:22 +0900

    Update 04_회귀분석.R

--- Script/04_회귀분석.R
+++ Script/04_회귀분석.R
@@ -1,4 +1,4 @@
-## 9
+## The CPI Data
 
 year <- rep(2008:2010, each = 4)
 quarter <- rep(1:4, 3)
@@ -6,7 +6,7 @@ cpi <- c(162.2,164.6,166.5,166,166.2,167,168.6,169.5,171,172.1,173.3,174)
 plot(cpi, xaxt = "n", ylab = "CPI", xlab = "")
 axis(1, labels = paste(year, quarter, sep = "Q"), at = 1:12, las = 3)
 
-## 10
+## Train by Linear Regression 1
 
 cor(year, cpi)
 cor(quarter, cpi)
@@ -16,7 +16,9 @@ fit
 cpi2011 <- fit$coefficients[[1]]+fit$coefficients[[2]]*2011 + fit$coefficients[[3]]*(1:4)
 cpi2011
 
-## 11
+# 𝑐𝑝𝑖=−7644.487500+3.887500∗𝑦𝑒𝑎𝑟+1.166667∗𝑞𝑢𝑎𝑟𝑡𝑒𝑟
+
+## Train by Linear Regression 2
 
 attributes(fit)
 
@@ -26,13 +28,7 @@ residuals(fit)
 
 summary(fit)
 
-## 12
-
-library(scatterplot3d)
-s3d <- scatterplot3d(year, quarter, cpi, cpi, highlight.3d = T, type = 'h', lab = c(2,3))
-s3d$plane3d(fit)
-
-## 13
+## Prediction of CPIs in 2011
 
 data2011 <- data.frame(year = 2011, quarter = 1:4)
 cpi2011 <- predict(fit, newdata = data2011)
@@ -40,14 +36,42 @@ style <- c(rep(1,12), rep(2,4))
 plot(c(cpi, cpi2011), xaxt = 'n', ylab = 'CPI', xlab = '', pch = style, col = style)
 axis(1, at = 1:16, las = 3, labels = c(paste(year, quarter, sep='Q'), '2011Q1', '2011Q2', '2011Q3','2011Q4'))
 
-## 16
+## Generalized Linear Model
 data('bodyfat', package='TH.data')
 myFormula <- DEXfat ~ age + waistcirc + hipcirc + elbowbreadth + kneebreadth
 bodyfat.glm <- glm(myFormula, family = gaussian('log'), data = bodyfat)
 summary(bodyfat.glm)
 
-## 17
-
 pred <- predict(bodyfat.glm, type = 'response')
 plot(bodyfat$DEXfat, pred, xlab = 'Observed', ylab = 'Prediction')
 abline(a = 0, b = 1, col = 'red', lwd = 2)
+
+
+## Build Logistic Regression Model
+
+# Examine the dataset to identify potential independent variables
+str(donors)
+
+# Explore the dependent variable
+table(donors$donated)
+
+# Build the donation model
+donation_model <- glm(donated ~ bad_address + interest_religion + interest_veterans, 
+                      data = donors, family = "binomial")
+
+# Summarize the model results
+summary(donation_model)
+
+## Binary Prediction
+
+# Estimate the donation probability
+donors$donation_prob <- predict(donation_model, type = "response")
+
+# Find the donation probability of the average prospect
+mean(donors$donated)
+
+# Predict a donation if probability of donation is greater than average (0.0504)
+donors$donation_pred <- ifelse(donors$donation_prob > 0.0504, 1, 0)
+
+# Calculate the model's accuracy
+mean(donors$donated == donors$donation_pred)
